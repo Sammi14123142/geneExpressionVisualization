5a3234b71c866a47120fca321adb640e2bc3cd1b
Sammi14123142 <14123142d@connect.polyu.hk>
2016-06-01 11:53:50 +0800

    cluster, pair plots, silplot

--- ui.R
+++ ui.R
@@ -1,10 +1,35 @@
 library(shiny)
 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
   
-  headerPanel(),
+  headerPanel("Interactive Multivariate Visualization Using Gene Expression Data"),
   
-  sidebarPanel(),
+  sidebarPanel(
+    helpText("Choose distance:"),
+    fluidRow(
+      selectInput("dmeth",NULL, choices=dmeths,
+                  selected = dmeths[1])),
+    helpText("Choose cluster method:"),
+    fluidRow(
+      selectInput("meth", NULL, choices=cmeths,
+                  selected=cmeths[1])),
+    helpText("Choose height of cut:"),
+    fluidRow(
+      numericInput("cutval", NULL, value=40, min=0,
+                   max=Inf, step=1)),
+    helpText(paste("Choose variables for clustering",
+                   substitute(df), ":")),
+    fluidRow(checkboxGroupInput("vars", NULL, choices=nms,
+                                selected=nms[1:2]))
+    ),
   
-  mainPanel()
+  mainPanel(
+    tabsetPanel(
+      tabPanel("tree", 
+               plotOutput("Cluster")),
+      tabPanel("pairs", 
+               plotOutput("pairsplot")),
+      tabPanel("silh", 
+               plotOutput("silplot"))
+    ))
 ))
